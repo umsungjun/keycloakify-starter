20c413e130e1b79cc73a590af93d8b12171170ef
umsungjun <99730910+umsungjun@users.noreply.github.com>
2025-11-19 10:45:57 +0900

    Autowini Admin 로그인 페이지 생성

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -4,9 +4,12 @@ import type { KcContext } from "./KcContext";
 import { useI18n } from "./i18n";
 import DefaultPage from "keycloakify/login/DefaultPage";
 import Template from "keycloakify/login/Template";
+
 const UserProfileFormFields = lazy(
     () => import("keycloakify/login/UserProfileFormFields")
 );
+/* Pages */
+const Login = lazy(() => import("./pages/Login"));
 
 const doMakeUserConfirmPassword = true;
 
@@ -19,6 +22,16 @@ export default function KcPage(props: { kcContext: KcContext }) {
         <Suspense>
             {(() => {
                 switch (kcContext.pageId) {
+                    case "login.ftl":
+                        return (
+                            <Login
+                                kcContext={kcContext}
+                                i18n={i18n}
+                                classes={classes}
+                                Template={Template}
+                                doUseDefaultCss={false}
+                            />
+                        );
                     default:
                         return (
                             <DefaultPage
